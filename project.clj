8ec9293c558c250cbc741568da9a042f5c1b67d6
Craig Andera <candera@wangdera.com>
2013-05-24 15:11:57 -0400

    Support attaching a debugger.

--- project.clj
+++ project.clj
@@ -9,4 +9,7 @@
                  [com.googlecode.soundlibs/mp3spi "1.9.5-1"]]
   :java-source-paths ["java"]
   :main podcastifier.main
-  :profiles {:dev {:source-paths ["dev"]}})
+  :profiles {:dev
+             {:source-paths ["dev"]
+              :jvm-opts ["-Xdebug"
+                         "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9900"]}})
