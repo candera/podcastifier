8b3b3ddff4280159820f9b8a395e9bb57033bc75
Craig Andera <candera@wangdera.com>
2013-06-16 16:47:04 -0400

    Update project.clj
    - Depend on dynne
    - Update tools.namespace to 0.2.3
    - Re-enable debugging

--- project.clj
+++ project.clj
@@ -4,12 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.namespace "0.2.3"]
-                 [incanter "1.4.0"]
-                 [com.googlecode.soundlibs/mp3spi "1.9.5-1"]]
-  :java-source-paths ["java"]
+                 [org.craigandera/dynne "0.1.0-SNAPSHOT"]]
   :main podcastifier.main
   :profiles {:dev
              {:source-paths ["dev"]
+              :dependencies [[org.clojure/tools.namespace "0.2.3"]]
               :jvm-opts ["-Xdebug"
-                         "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9900"]}})
+                         "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9900"]
+              }})
