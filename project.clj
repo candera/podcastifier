fe5a99cfd89e2cd992a07c513bec0b932c40a6ac
Craig Andera <candera@wangdera.com>
2013-06-23 08:26:44 -0400

    Update to latest version of dynne and bump project version.

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject org.craigandera/podcastifier "0.1.0"
+(defproject org.craigandera/podcastifier "0.2.0-SNAPSHOT"
   :description "Automates audio mixing of podcast elements into the final product"
   ;; :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.craigandera/dynne "0.1.0-SNAPSHOT"]]
+                 [org.craigandera/dynne "0.3.0-SNAPSHOT"]]
   :main podcastifier.main
   :profiles {:dev
              {:source-paths ["dev"]
               :dependencies [[org.clojure/tools.namespace "0.2.3"]]
-              :jvm-opts ["-Xdebug"
-                         "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9900"]
+              :jvm-opts ^:replace ["-Xdebug"
+                                   "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9900"]
               }})
