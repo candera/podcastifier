f578fc929c9cdb1c032d381eba949a71c0a52a25
Craig Andera <candera@wangdera.com>
2013-08-11 11:44:35 -0400

    Add Prismatic's hiphip as a dependency.

--- project.clj
+++ project.clj
@@ -4,11 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.craigandera/dynne "0.3.0-SNAPSHOT"]]
+                 [org.craigandera/dynne "0.3.0-SNAPSHOT"]
+                 [prismatic/hiphip "0.1.0"]]
   :main podcastifier.main
   :profiles {:dev
              {:source-paths ["dev"]
               :dependencies [[org.clojure/tools.namespace "0.2.3"]]
               :jvm-opts ^:replace ["-Xdebug"
-                                   "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9900"]
+                                   ;;"-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9900"
+                                   ]
               }})
