4884d57f16d929daf65392ec004ee04eb20a1ada
Craig Andera <candera@wangdera.com>
2013-08-30 17:18:03 -0400

    Update dynne dependency to 0.3.0. Update project version to 0.3.0.

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
-(defproject org.craigandera/podcastifier "0.2.0-SNAPSHOT"
+(defproject org.craigandera/podcastifier "0.3.0"
   :description "Automates audio mixing of podcast elements into the final product"
   ;; :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.craigandera/dynne "0.3.0-SNAPSHOT"]
-                 ;;[prismatic/hiphip "0.1.0"]
-                 ]
+                 [org.craigandera/dynne "0.3.0"]]
   :main podcastifier.main
   :profiles {:dev
              {:source-paths ["dev"]
